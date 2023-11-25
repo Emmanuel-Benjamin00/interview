d5f98a7f3062c0001ee33549304b1387fa01fcd5
Emmanuel Benjamin D <emmanuel26112000@gmail.com>
2023-11-25 12:21:59 +0530

    changed card to teaxtare

--- src/components/AnswerCard.jsx
+++ src/components/AnswerCard.jsx
@@ -2,21 +2,17 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 
-function AnswerCard(props){
+function AnswerCard(props) {
     const navigate = useNavigate()
-     return (
-         <>
-             <div className="container-md page ps-2 ms-1 mt-4">
-                 <div className="card text-bg-light  ms-4" style={{ width: "28rem",  maxHeight: "fit-content" }}>
-                     <div className="card-body">
-                         <h5 className="card-title">{props.quesans.ques}</h5>
-                         <p className="card-text">{props.quesans.ans}</p>
-                     </div>
-                 </div>
-             </div>
-             <Button style={{height:"fit-content"}} onClick={()=>navigate("/data")}>Data</Button>
-         </>
-     )
- }
+    return (
+        <>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">{props.quesans.ques}</label>
+                <textarea class=" ms-4 form-control" id="exampleFormControlTextarea1" rows="3" value={props.quesans.ans}  style={{ width: "28rem", maxHeight: "fit-content", minHeight:"30rem", background:"white" }} disabled></textarea>
+            </div>
+            <Button style={{ height: "fit-content" }} onClick={() => navigate("/data")}>Data</Button>
+        </>
+    )
+}
 
 export default AnswerCard
